ccb3602bff1b3fb5d6c4d0375c97cf093e6458e4
HustlerDev45 <abdoul4845@gmail.com>
2024-01-21 10:20:50 -0800

    sort.h fixed

--- sort.h
+++ sort.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 /**
  * struct listint_s - Doubly linked list node
  *
