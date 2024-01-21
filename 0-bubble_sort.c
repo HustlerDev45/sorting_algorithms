f46e2aa7dfa2e9ed42a533c7a1d4cfd54263f996
HustlerDev45 <abdoul4845@gmail.com>
2024-01-21 12:53:17 -0800

    t0-F

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,39 +21,35 @@ void swap_int(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n = size;
-	int swp;
+        size_t i, j, n = size;
+        int swp;
 
-	if (array == NULL || size < 2)
-		return;
+        if (array == NULL || size < 2)
+                return;
 
-	while (1)
-	{
-		swp = 0;
+        while (1)
+        {
+                swp = 0;
 
-		for (i = 0; i < n - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_int(&array[i], &array[i + 1]);
-				swp = 1;
-			}
-		}
+                for (i = 0; i < n - 1; i++)
+                {
+                        if (array[i] > array[i + 1])
+                        {
+                                swap_int(&array[i], &array[i + 1]);
+                                for (j = 0; j < size - 1; j++)
+                                {
+                                        printf("%d, ", array[j]);
+                                }
+                                printf("%d\n", array[size - 1]);
+                                swp = 1;
+                        }
+                }
 
-		if (swp)
-		{
-			for (i = 0; i < size - 1; i++)
-			{
-				printf("%d, ", array[i]);
-			}
-			printf("%d\n", array[size - 1]);
-		}
+                if (!swp)
+                {
+                        break;
+                }
 
-		if (!swp)
-		{
-			break;
-		}
-
-		n--;
-	}
+                n--;
+        }
 }
